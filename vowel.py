fa4b883e5f1d9ce26aef0795cbac8aa39e2d2121
Pallavi2000 <53207976+Pallavi2000@users.noreply.github.com>
2019-07-25 17:03:50 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,14 @@
+"""program to accept an input string from the user 
+and determine the vowels in the string and 
+calculate the number of vowels."""
+
+lst = ['a','e','i','o','u']
+
+s = input("Enter a string : ")
+s = s.lower()
+
+res = list(filter(lambda x:x in lst,s)
+
+print(f" All the vowels in the given string {s} is {res}")
+
+print(f"Vowel count = {len(res)}")
